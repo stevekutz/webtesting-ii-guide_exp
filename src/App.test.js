6669968457520411db8ca0a4e1fc119f40b67281
stevekutz <stkutz@gmail.com>
2019-07-12 16:56:40 -0500

    added react testing library, verified render works

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {render} from '@testing-library/react'; // ONLY NEED THIS ONE !!!
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('here comes <App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('render without errors', () => {
+    // if there are errors rendering the component, it will fail
+    render(<App/>);
+  })
+
+  it('render with failing', () => {
+   // if there are errors rendering the component, it will fail
+    render(<App/>);
+  } )
+
+})
+
+
