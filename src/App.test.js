ca38607489ec5e0809b46599323dabc8adcc2126
stevekutz <stkutz@gmail.com>
2019-07-12 18:02:23 -0500

    added destructuring with getByText

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {render} from '@testing-library/react'; // ONLY NEED THIS ONE !!!
+import '@testing-library/react/cleanup-after-each'; // Automatically resets render for each test
 
 describe('here comes <App />', () => {
   it('renders without crashing', () => {
@@ -19,6 +20,20 @@ describe('here comes <App />', () => {
    // if there are errors rendering the component, it will fail
     render(<App/>);
   } )
+
+  it.todo('does this work?');
+
+  it('should show an h2 component with text "Something" ', () => {
+    const testDOM = render(<App/>);
+    testDOM.getByText(/something/i);  // i means case-insenstive
+  })
+
+  // ANOTHER way to use getByText
+  it('ALT WAY >> should shows an h2 component with text "something"', () => {
+    const {getByText} = render(<App/>);  // deconstructed !!!
+    getByText(/something/i);
+  })
+
 
 })
 
